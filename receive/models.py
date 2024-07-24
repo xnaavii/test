ad081890b1692993c6785f1ca17a854ad2fabb5a
Ivan <petrovic.valpovo@gmail.com>
2024-07-24 21:56:46 +0100

    Add option to view the map, integrate leaftjet and by clicking points out the map location

--- receive/models.py
+++ receive/models.py
@@ -5,3 +5,6 @@ class GPSTracker(models.Model):
     timestamp = models.DateTimeField(auto_now=True)
     longitude = models.FloatField()
     latitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.latitude}, {self.longitude}, {self.timestamp}"
